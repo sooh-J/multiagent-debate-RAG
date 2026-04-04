8a6fd651523954d6ab55b3adef1995ac4d9c4b16
sooh-J <suhyeonjun@postech.ac.kr>
2026-04-04 18:28:48 +0900

    Increase evaluation sample size from 20 to 100

--- run_madamrag.py
+++ run_madamrag.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     sys.stdout = tee
 
     try:
-        ds_sample = load_ramdocs(n_samples=20)
+        ds_sample = load_ramdocs(n_samples=100)
         all_results = run_on_dataset(ds_sample)
 
         print_usage_summary()
